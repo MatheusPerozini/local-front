f782b639cc81b33d02b9a33bbdd5c439bea81060
Matheus Perozini Magalhães <perozinimagalhesmatheus@gmail.com>
2025-12-06 21:38:43 -0300

    Enable reload

--- app/api/index.tsx
+++ app/api/index.tsx
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: process.env.BASE_API_URL,
+  headers: {
+    'Cache-Control': 'no-cache, no-store, must-revalidate'
+  }
 });
